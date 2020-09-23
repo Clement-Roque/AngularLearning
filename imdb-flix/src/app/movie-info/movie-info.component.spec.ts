faef1f3a69ebc5167470587268d5c5a40d5d7541
clem <clement.eberoque@gmail.com>
2020-09-23 09:20:29 +0200

    test put in practice, lower image size

--- imdb-flix/src/app/movie-info/movie-info.component.spec.ts
+++ imdb-flix/src/app/movie-info/movie-info.component.spec.ts
@@ -1,25 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MovieInfoComponent } from './movie-info.component';
+import { Movie} from '../movie';
+import { MOVIES } from '../mock-movies';
 
 describe('MovieInfoComponent', () => {
   let component: MovieInfoComponent;
   let fixture: ComponentFixture<MovieInfoComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ MovieInfoComponent ]
-    })
-    .compileComponents();
-  });
+   beforeEach(async () => {
+     await TestBed.configureTestingModule({
+       declarations: [ MovieInfoComponent ]
+     })
+     .compileComponents();
+   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(MovieInfoComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+
   });
 
   it('should create', () => {
+    component.movie =  new Movie(MOVIES[0]);
     expect(component).toBeTruthy();
   });
 });
